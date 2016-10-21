f6f3eed6f355e2d593d3128783363a03c463d57f
Sergio Uve <sergiouve@gmail.com>
2016-10-21 09:20:11 +0200

    Colors babe

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
-$(document).ready(function{
+$(document).ready(function() {
 
-    var $main = $('main');
-
-    var initNeonName = function() {
-        var $nameElement = $main.find('js-name-box');
+    String.prototype.replaceAt = function(index, character) {
+        return this.substr(0, index) + character + this.substr(index + character.length);
     }
 
+    var $main = $('main');
+    var $nameElement = $main.find('.js-name-box');
+
+    var getRandomHexColor = function() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+    var surrondCharsWithSpan = function($element) {
+        var elementChars = $element.text();
+
+        for (var i = 0, length = elementChars.length; i < length; i++) {
+            elementChars[i] = elementChars.replaceAt(i, '<span class="char">' + elementChars[i] + '</span>');
+        }
+        return elementChars;
+    }
+
+    var spiceNameUp = function() {
+        $nameElement.css('color', getRandomHexColor());
+    }
+
+    $nameElement.html(surrondCharsWithSpan($nameElement));
+
+    window.setInterval(function() {
+        spiceNameUp();
+    }, 50);
 });
 
