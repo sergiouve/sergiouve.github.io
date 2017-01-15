c643a5ca13edae586f39347108532963e7fd3fe5
Sergio Uve <sergiouve@gmail.com>
2017-01-15 18:49:31 +0100

    Add raw logic to help modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,30 @@
 var terminal = require('./terminal.js');
 
 $(document).ready(function() {
+
+    var showHelpModal = function() {
+        var $modalSection = $('#help-modal');
+        var $modalHelpBox = $modalSection.find('.js-help-container');
+
+        $modalSection.show();
+        $modalHelpBox.animate({
+            top: '+=900'
+        }, 400);
+    };
+
+    var hideHelpModal = function() {
+
+    }
+
+    var initHelpButton = function() {
+        var $helpButton = $('.js-help');
+
+        $helpButton.on('click', function() {
+            showHelpModal();
+        });
+    };
+
+    initHelpButton();
     terminal.initTerminalListener();
 });
 
