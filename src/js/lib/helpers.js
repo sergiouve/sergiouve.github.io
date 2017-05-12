8bb0962bba27cb3a7b79e85bc292d1c7c4f0b244
Sergio Uve <sergiouve@gmail.com>
2017-05-12 15:08:06 +0200

    Format helpers file

--- src/js/lib/helpers.js
+++ src/js/lib/helpers.js
@@ -1,29 +1,32 @@
 var helpers = {
 
-    // TODO
-    getParentFolder: function(tree, pathList) {
-        var needle = pathList[pathList.length - 1];
-        for (level in tree) {
-            if (level == needle){
-                return tree[level];
-            } else if (!!tree[level] && typeof (tree[level]) == "object") {
-                console.log(level, tree[level])
-                helpers.getParentFolder(tree[level], pathList);
-            }
-        }
-    },
+  // TODO
+  getParentFolder: function(tree, pathList) {
+    var needle = pathList[pathList.length - 1];
 
-    updateCurrentPath: function(currentPath, directory) {
-        currentPath.push(directory);
-        return currentPath;
-    },
-
-    formatCurrentPathString: function(currentPath) {
-        currentPath = currentPath.toString();
-        currentPath = currentPath.replace(/,/g, '/');
-        currentPath = currentPath.replace(/\/\//g, '/');
-        return currentPath;
+    for (level in tree) {
+      if (level == needle) {
+        return tree[level];
+      } else if (!!tree[level] && typeof (tree[level]) == "object") {
+        console.log(level, tree[level])
+        helpers.getParentFolder(tree[level], pathList);
+      }
     }
+  },
+
+  updateCurrentPath: function(currentPath, directory) {
+    currentPath.push(directory);
+
+    return currentPath;
+  },
+
+  formatCurrentPathString: function(currentPath) {
+    currentPath = currentPath.toString();
+    currentPath = currentPath.replace(/,/g, '/');
+    currentPath = currentPath.replace(/\/\//g, '/');
+
+    return currentPath;
+  }
 
 }
 
